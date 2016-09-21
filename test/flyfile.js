accbd18619fb2fb386f4d9820c72ef6255596949
Tyler Benton <tjbenton21@gmail.com>
2016-09-21 14:44:35 -0400

    added a more real world test case (#2)

---
+++ test/flyfile.js
@@ -0,0 +1,5 @@
+export { a, b } from './include.js'
+
+export default async function() {
+  console.log('default');
+}
---
+++ test/include.js
@@ -0,0 +1,7 @@
+export async function a() {
+  console.log('a')
+}
+
+export async function b() {
+  console.log('b');
+}
